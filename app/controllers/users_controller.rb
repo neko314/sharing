8148c5df79e2a7ae17ff7564b2e70c75774121ab
keiko <keiko.cda@gmail.com>
2019-02-23 22:49:13 +0900

    Add Users#new create show destroy

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(user_params)
+    if @user.destroy
+      redirect_to new_user_path
+    else
+      render user_path(@user)
+    end
+  end
+
+  private
+   def user_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   end
+end
