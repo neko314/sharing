caaadd777320eed7af7946b005edd76113f0df75
keiko <keiko.cda@gmail.com>
2019-02-23 21:21:18 +0900

    Create groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_121741) do
+ActiveRecord::Schema.define(version: 2019_02_23_121937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_02_23_121741) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_02_23_121741) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "groups", "users"
 end
