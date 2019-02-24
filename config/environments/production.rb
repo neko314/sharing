7d4dc78da12b43e93bbc548ffff372fa09944397
keiko <keiko.cda@gmail.com>
2019-02-24 15:41:29 +0900

    Change mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,12 +94,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
-  }
 end
