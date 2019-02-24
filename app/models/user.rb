57b62badb40e54edea252afa858c89ae3b18d41b
keiko <keiko.cda@gmail.com>
2019-02-24 12:19:54 +0900

    Fix syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable,
+         :confirmable
 end
