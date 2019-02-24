a37676708ee558000ceec9c43d02d9678d7de051
keiko <keiko.cda@gmail.com>
2019-02-24 20:15:50 +0900

    Fix groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,12 +7,19 @@ class GroupsController < ApplicationController
 
   def create
     @group = current_user.groups.new(group_params)
-    @group.user_id = current_user.id
     if @group.save
       redirect_to user_path(current_user), notice: "New group created successfully"
     else
       render "new", notice: "Creating new group failed"
     end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def index
+    @groups = current_user.groups
   end
 
   private
