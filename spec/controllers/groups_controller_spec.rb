26bd0562a4e9ecaafefa8fc4a25e7eedade34f5b
keiko <keiko.cda@gmail.com>
2019-02-24 21:12:46 +0900

    Add test suits

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -3,4 +3,19 @@
 require "rails_helper"
 
 RSpec.describe GroupsController, type: :controller do
+  context "#create" do
+    it "新規登録できること" do
+    end
+    it "ログインユーザーが紐付いていること" do
+    end
+    it "ユーザーページにリダイレクトすること" do
+    end
+  end
+
+  context "名前が空欄の場合" do
+    it "登録できないこと" do
+    end
+    it "新規作成ページに戻ること" do
+    end
+  end
 end
