e18b5d571c66ebacc10b8edbab393eeb8f209604
keiko <keiko.cda@gmail.com>
2019-02-24 19:54:43 +0900

    Add groups#new #create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,20 @@
 class GroupsController < ApplicationController
   def new
-    @group = Group.new
+    @group = current_user.groups.new
+  end
+
+  def create
+    @group = current_user.groups.new(group_params)
+    @group.user_id = current_user.id
+    if @group.save
+      redirect_to user_path(current_user), notice: "New group created successfully"
+    else
+      render "new", notice: "Creating new group failed"
+    end
   end
 
   private
     def group_params
-      params.requore(:group).permit(:name)
+      params.require(:group).permit(:name)
     end
 end
