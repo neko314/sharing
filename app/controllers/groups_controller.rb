7b6d1ad9d0aad2fe08b399bd2c38475781be65e2
keiko <keiko.cda@gmail.com>
2019-02-24 19:54:58 +0900

    Rubocop

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GroupsController < ApplicationController
   def new
     @group = current_user.groups.new
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 class Group < ApplicationRecord
   has_and_belongs_to_many :users
-  
 end
