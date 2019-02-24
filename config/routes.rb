d2540281473072fa131be01603a65bd5f7c2a830
keiko <keiko.cda@gmail.com>
2019-02-24 15:22:14 +0900

    Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root to: "home#show"
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: "users/sessions"
   }
   resources :users
 end
