4b7d1064912035a0b3f8ff13fc942e1a53f72918
keiko <keiko.cda@gmail.com>
2019-02-26 00:24:48 +0900

    Add day_ids to task_params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
 
   private
     def task_params
-      params.require(:task).permit(:name, days_id: [])
+      params.require(:task).permit(:name, day_ids: [])
     end
 
     def correct_user
