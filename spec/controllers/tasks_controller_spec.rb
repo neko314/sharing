c5f701515d499b84db1fc408964c3895b9437054
keiko <keiko.cda@gmail.com>
2019-02-25 22:51:03 +0900

    Change tasks tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,45 +3,13 @@
 require "rails_helper"
 
 RSpec.describe TasksController, type: :controller do
-  describe "GET #new" do
-    xit "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
+  context "ログインユーザーがグループのメンバーの場合" do
   end
 
-  describe "GET #create" do
-    xit "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  context "ログインユーザーがグループのメンバーではない場合" do
+    it "メッセージが表示されること" do
     end
-  end
-
-  describe "GET #show" do
-    xit "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    xit "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    xit "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    xit "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+    it "プロフィールページにリダイレクトされること" do
     end
   end
 end
