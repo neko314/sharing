7e0c310eb505c4496717f668cd34d50a6e6e2757
keiko <keiko.cda@gmail.com>
2019-02-25 22:36:02 +0900

    Add test suits

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -4,18 +4,48 @@ require "rails_helper"
 
 RSpec.describe GroupsController, type: :controller do
   context "#create" do
-    it "新規登録できること" do
+    context "名前が入力されている場合" do
+      it "新規登録できること" do
+      end
+      it "ログインユーザーが紐付いていること" do
+      end
+      it "ユーザーページにリダイレクトすること" do
+      end
     end
-    it "ログインユーザーが紐付いていること" do
+
+    context "名前が空欄の場合" do
+      it "登録できないこと" do
+      end
+      it "新規作成ページに戻ること" do
+      end
     end
-    it "ユーザーページにリダイレクトすること" do
+  end
+  
+  context "ログインユーザーがグループのメンバーの場合" do
+    context "#show" do
+      it "ページが表示されること" do
+      end
+    end
+    context "#edit" do
+      it "編集ページが表示されること" do
+      end
+    end
+    context "#update" do
+      it "情報が更新されること" do
+      end
+    end
+    context "#destroy" do
+      it "削除されること" do
+      end
     end
   end
 
-  context "名前が空欄の場合" do
-    it "登録できないこと" do
+  context "ログインユーザーがグループのメンバーでない場合" do
+    it "ページが表示されないこと" do
     end
-    it "新規作成ページに戻ること" do
+    it "メッセージが表示されること" do
+    end
+    it "プロフィールページにリダイレクトされること" do
     end
   end
 end
