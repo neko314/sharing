4c1c13a6bf6be9f5d269012ac6bfe31de301f52a
keiko <keiko.cda@gmail.com>
2019-02-27 07:03:36 +0900

    Associate tasks and assignments

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,6 @@ class Task < ApplicationRecord
   belongs_to :group
   has_and_belongs_to_many :days
   has_many :users, through: :assignments
+  has_many :assignments
+  accepts_nested_attributes_for :assignments, reject_if: :all_blank
 end
