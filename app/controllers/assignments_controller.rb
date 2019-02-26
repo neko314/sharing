439455a70a8c79c5476c5b752a33bfd8b47a7995
keiko <keiko.cda@gmail.com>
2019-02-26 22:56:36 +0900

    Change creating assignments form to save all at once

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,5 +1,6 @@
 class AssignmentsController < ApplicationController
   def new
+    @group = Group.find(params[:group_id])
     @assignment = Assignment.new
   end
 
@@ -12,5 +13,9 @@ class AssignmentsController < ApplicationController
   private
     def assignment_params
       params.require(:assignment).permit(:task_id, :user_id)
+    end
+
+    def assignments_params
+      params.require(:assignments).map { |assignment_params| assignment_params.permit(:task_id, :user_id) }
     end
 end
