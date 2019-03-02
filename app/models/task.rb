c2ef095cdcd7025c27542e18dcf8842342fadb02
keiko <keiko.cda@gmail.com>
2019-03-02 22:45:35 +0900

    Sort validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,8 @@
 class Task < ApplicationRecord
   belongs_to :group
   has_many :users, through: :assignments
-  has_many :days, through: :assignments
   has_many :assignments, dependent: :destroy
   accepts_nested_attributes_for :assignments, allow_destroy: true, reject_if: :all_blank
   validates :name, presence: true
+  has_many :days, through: :assignments
 end
