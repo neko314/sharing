20bb10ea70d7c421423e5efe82dd5aca528d3b44
keiko <keiko.cda@gmail.com>
2019-03-02 19:07:29 +0900

    Add test suites

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Task, type: :model  do
+  context "名前・曜日が入力されている時" do
+    it "タスクが登録されること" do
+    end
+  end
+
+  context "曜日が選択されていない場合" do
+    it "タスクが登録されないこと" do
+    end
+    
+    it "エラーメッセージが表示されること" do
+    end
+  end
+end
