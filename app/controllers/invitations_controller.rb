73079082e892a54ded2cd7377ded461d9aaf1828
keiko <keiko.cda@gmail.com>
2019-03-03 09:34:04 +0900

    Change inivitaions

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class InvitationsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     user = User.find_by(invitation_params)
-    binding.irb
     if user.present?
-      @group.users << user, notice: "Inivited #{user.name}"
+      @group.users << user
+      redirect_to @group, notice: "Inivited #{user.name}"
     else
-      render @group, notice: "Couldn't find user"
+      render group_path(@group), alert: "Couldn't find user"
     end
   end
 
