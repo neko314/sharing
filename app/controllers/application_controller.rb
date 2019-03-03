47c9d0a95d18c73f8f37e0ef806a807a27692150
keiko <keiko.cda@gmail.com>
2019-03-03 10:21:29 +0900

    Setting for sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_raven_context
 
   private
     def after_sign_in_path_for(user)
@@ -17,5 +18,12 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+  private
+
+    def set_raven_context
+      Raven.user_context(id: session[:current_user_id])
+      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
     end
 end
