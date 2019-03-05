d5930ed7d30bb75b2468c6b4e8808f3d95b40d22
keiko <keiko.cda@gmail.com>
2019-03-05 22:49:16 +0900

    Change top page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    render new_user_session_path unless user_signed_in?
+    current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: "home#show"
+  root to: "users#show"
   devise_for :users, controllers: {
     sessions: "users/sessions"
   }
