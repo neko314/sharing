008788eab598d2f227a2aff9076c0ab54c333f71
keiko <keiko.cda@gmail.com>
2019-03-05 23:23:17 +0900

    Fix users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,5 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    render new_user_session_path unless user_signed_in?
-    current_user
   end
 end
