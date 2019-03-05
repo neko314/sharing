099b5a286daa48e59235d4fe74a420497ff72e81
keiko <keiko.cda@gmail.com>
2019-03-05 22:12:07 +0900

    Modify group page

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -4,14 +4,12 @@ class AssignmentsController < ApplicationController
   def edit
     @group = Group.find(params[:group_id])
     @assignment = Assignment.find(params[:id])
-    # binding.pry
   end
 
   def update
     @assignment = Assignment.find(params[:id])
-    
+
     if @assignment.update(assignment_params)
-      binding.pry
       redirect_to group_path(@assignment.task.group), notice: "Updated assignment successfully"
     else
       render "edit", alert: "Failed to update assignment"
@@ -21,9 +19,5 @@ class AssignmentsController < ApplicationController
   private
     def assignment_params
       params.require(:assignment).permit(:task_id, :user_id)
-    end
-
-    def assignments_params
-      params.require(:assignments).map { |assignment_params| assignment_params.permit(:task_id, :user_id) }
     end
 end
