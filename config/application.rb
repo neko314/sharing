2825cced7c129086f8f72a7d982a7e600fc5cff4
keiko <keiko.cda@gmail.com>
2019-03-06 21:45:18 +0900

    Change spec file generator configuration

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Sharing
       g.test_framework :rspec, 
         view_specs: false, 
         helper_specs: false, 
-        controller_specs: false, 
         routing_specs: false
     end
   end
