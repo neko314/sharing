a76fc610cfd09f39b5e38c16bff071d6da4836ee
keiko <keiko.cda@gmail.com>
2019-03-06 21:52:52 +0900

    Rubocop

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,9 @@ module Sharing
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
-      g.test_framework :rspec, 
-        view_specs: false, 
-        helper_specs: false, 
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
         routing_specs: false
     end
   end
