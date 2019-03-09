e022761e2ac6b99abeeb2e5f2b63ddea3b391927
keiko <keiko.cda@gmail.com>
2019-03-09 21:40:41 +0900

    Rubocop

--- spec/controllers/groups/memberships_controller_spec.rb
+++ spec/controllers/groups/memberships_controller_spec.rb
@@ -5,8 +5,8 @@ require "rails_helper"
 RSpec.describe Groups::MembershipsController, type: :controller do
   describe "#create" do
     context "user is find with given email"
-      it "add the user to group" do
-      end
+    it "add the user to group" do
+    end
     context "user is not found" do
       it "add no user to group" do
       end
