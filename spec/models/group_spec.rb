03238e9f483521128aa8192efbdf60ae6565380d
keiko <keiko.cda@gmail.com>
2019-03-09 11:36:41 +0900

    Add group model tests

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,20 @@
+
+require "rails_helper"
+
+RSpec.describe Group, type: :model  do
+  before do
+    @user = FactoryBot.build(:user)
+    @group = @user.groups.new
+  end
+
+  it "is valid with name" do
+    @group.name = "Group"
+    expect(@group).to be_valid
+  end
+
+  it "is invalid without name" do
+    @group.name = ""
+    @group.valid?
+    expect(@group.errors[:name]).to include("can't be blank")
+  end
+end
