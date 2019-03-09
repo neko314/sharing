607fcbc2620cb5a107d97809c704c50be4a2fdf4
keiko <keiko.cda@gmail.com>
2019-03-09 21:39:26 +0900

    Modify test

---
+++ spec/controllers/groups/memberships_controller_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Groups::MembershipsController, type: :controller do
+  describe "#create" do
+    context "user is find with given email"
+      it "add the user to group" do
+      end
+    context "user is not found" do
+      it "add no user to group" do
+      end
+    end
+  end
+  describe "#destroy" do
+    it "exit from group" do
+    end
+  end
+end
