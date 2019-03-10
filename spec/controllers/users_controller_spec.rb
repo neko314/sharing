7500d62047f4c3b22a9793ebc002e56a7e69fe91
keiko <keiko.cda@gmail.com>
2019-03-10 13:09:11 +0900

    Add users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,18 +3,23 @@
 require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
-  context "ログイン済み場合" do
-    context "他ユーザーの情報" do
-      it "プロフィールページを表示すると自分のページにリダイレクトされること" do
-      end
-      it "プロフィールを編集するとログインページにリダイレクトされること" do
-      end
-      it "削除するとログインページにリダイレクトされること" do
+  # @request.env['devise.mapping'] = Devise.mappings[:user]
+
+  fixtures :users
+  describe "#show" do
+    context "user signed in" do
+      it "responds successfully" do
+        sign_in users(:user1)
+        get :show
+        expect(response).to be_truthy
       end
     end
-  end
-  context "ログインしていない場合" do
-    it "ログインページにリダイレクトされること" do
+
+    context "user not signed in" do
+      it "redirects to sign in page" do
+        get :show
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
