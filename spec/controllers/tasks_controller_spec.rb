5529c92079cb8aee4dfe510edbe108869208b8b4
keiko <keiko.cda@gmail.com>
2019-03-10 21:52:16 +0900

    Add test descriptions

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,13 +3,105 @@
 require "rails_helper"
 
 RSpec.describe TasksController, type: :controller do
-  context "ログインユーザーがグループのメンバーの場合" do
+  before do
   end
 
-  context "ログインユーザーがグループのメンバーではない場合" do
-    it "メッセージが表示されること" do
+  describe "#new" do
+    context "as a correct user" do
+      it "responds successfully" do
+      end
     end
-    it "プロフィールページにリダイレクトされること" do
+    context "as a wrong user" do
+      it "redirects to top page" do
+      end
+    end
+    context "as a guest" do
+      it "redirects to sign in page" do
+      end
+    end
+  end
+
+  describe "#create" do
+    context "as a correct user" do
+      it "adds a new task" do
+      end
+      it "creates assingmnent of each day" do
+      end
+    end
+    context "as a wrong user" do
+      it "can't add a new task" do
+      end
+      it "redirects to top page" do
+      end
+    end
+    context "as a guest" do
+      it "can't add a new task" do
+      end
+      it "redirects to sign in page" do
+      end
+    end
+  end
+
+  describe "#edit" do
+    context "as a correct user" do
+      it "responds successfully" do
+      end
+    end
+    context "as a wrong user" do
+      it "redirects to top page" do
+      end
+    end
+    context "as a guest" do
+      it "redirects to sign in page" do
+      end
+    end
+  end
+
+  describe "#update" do
+    context "as a correct user" do
+      context "to change name" do
+        it "updates task name" do
+        end
+      end
+      context "selection more day" do
+        it "adds days to task" do
+        end
+      context "cancel a day" do
+        it "remove day from task" do
+        end
+      end
+    end
+    context "as a wrong user" do
+      it "can't update day" do
+      end
+      it "redirects to top page" do
+      end
+    end
+    context "as a guest" do
+      it "can't update day" do
+      end
+      it "redirects to sign in page" do
+      end
+    end
+  end
+  describe "#destroy" do
+    context "as a correct user" do
+      it "deletes a task" do
+      end
+      it "deletes assignments too" do
+      end
+    end
+    context "as a wrong user" do
+      it "can't delete a task" do
+      end
+      it "redirects to top page" do
+      end
+    end
+    context "as a guest" do
+      it "can't delete a task" do
+      end
+      it "redirects to sign in page" do
+      end
     end
   end
 end
