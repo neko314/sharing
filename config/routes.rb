b28280de0ef30d9e91bedd5ef35eb34f82b052ed
keiko <keiko.cda@gmail.com>
2019-03-11 20:19:10 +0900

    Change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   resources :groups do
     resources :tasks
     resources :assignments
-    post "invitation", to: "groups/memberships#create", as: :invitation
-    get "exit", to: "groups/memberships#destroy", as: :exit
+    resources :memberships, only: [:create, :destroy], module: "groups"
   end
 end
