944c93064e3093b7f284fbd04f67db95d4e20aa6
keiko <keiko.cda@gmail.com>
2019-03-15 22:04:34 +0900

    Rubocop

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   def update
-    if current_user.update()
-    redirect_to root_path
+    if current_user.update
+      redirect_to root_path
+    end
   end
 end
