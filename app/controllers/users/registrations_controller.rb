d30bb953c8badb8e3dfbe7a25eca53dfd1909320
keiko <keiko.cda@gmail.com>
2019-03-15 22:20:36 +0900

    Fix parameter

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Users::RegistrationsController < Devise::RegistrationsController
-  def update
-    if current_user.update
-      redirect_to root_path
-    end
-  end
-end
