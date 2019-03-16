134140963fc164877dc4b79812ac49628ea4553a
keiko <keiko.cda@gmail.com>
2019-03-16 15:36:16 +0900

    Add set_group method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,30 +3,27 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user
+  before_action :set_group
   before_action :set_task, only: [:edit, :update, :destroy]
 
   def new
-    @group = Group.find(params[:group_id])
     @task = Task.new
     Day.count.times { @task.assignments.new }
   end
 
   def create
-    group = Group.find(params[:group_id])
-    @task = group.tasks.new(task_params)
+    @task = @group.tasks.new(task_params)
     if @task.save
-      redirect_to group_path(@task.group), notice: "New task created successfully"
+      redirect_to group_path(@group), notice: "New task created successfully"
     else
       render "new", alert: "Creating new task failed"
     end
   end
 
   def edit
-    @group = Group.find(params[:group_id])
   end
 
   def update
-    @group = Group.find(params[:group_id])
     if @task.update(task_params)
       redirect_to group_path(@group), notice: "Update task successfully"
     else
@@ -46,6 +43,10 @@ class TasksController < ApplicationController
     def correct_user
       group = Group.find(params[:group_id])
       redirect_to root_path, alert: "You can't access" unless group.user_ids.include?(current_user.id)
+    end
+
+    def set_group
+      @group = Group.find(params[:group_id])
     end
 
     def set_task
