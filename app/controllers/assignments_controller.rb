9d44ab3a55eac84bc477ff838bab5b898d504c42
keiko <keiko.cda@gmail.com>
2019-03-16 15:06:55 +0900

    Add set_assignment method

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -3,14 +3,13 @@
 class AssignmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user
+  before_action :set_assignment
 
   def edit
     @group = Group.find(params[:group_id])
-    @assignment = Assignment.find(params[:id])
   end
 
   def update
-    @assignment = Assignment.find(params[:id])
     if @assignment.update(assignment_params)
       redirect_to group_path(@assignment.task.group), notice: "Updated assignment successfully"
     else
@@ -21,6 +20,10 @@ class AssignmentsController < ApplicationController
   private
     def assignment_params
       params.require(:assignment).permit(:task_id, :user_id)
+    end
+
+    def set_assignment
+      @assignment = Assignment.find(params[:id])
     end
 
     def correct_user
