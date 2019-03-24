488824d01f3e96e7ff554d611cb06723b5ba980e
keiko <keiko.cda@gmail.com>
2019-03-24 18:02:36 +0900

    Add unique constraint to group name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_024218) do
+ActiveRecord::Schema.define(version: 2019_03_24_085706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_03_24_024218) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
   create_table "groups_users", id: false, force: :cascade do |t|
