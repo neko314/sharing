f1ae4cc928b1919687807a39c3f9d4ba493313e7
keiko <keiko.cda@gmail.com>
2019-03-24 20:02:32 +0900

    Add not null constraint to days

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_093135) do
+ActiveRecord::Schema.define(version: 2019_03_24_110134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_03_24_093135) do
   end
 
   create_table "days", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
   end
 
   create_table "groups", force: :cascade do |t|
