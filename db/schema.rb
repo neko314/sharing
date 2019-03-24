fc2c36c44a7ca5bca1a98c4afc8e64ba514880bd
keiko <keiko.cda@gmail.com>
2019-03-24 20:27:50 +0900

    Add not null constraint to assignments

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_110134) do
+ActiveRecord::Schema.define(version: 2019_03_24_110314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "assignments", force: :cascade do |t|
     t.bigint "user_id"
-    t.bigint "task_id"
+    t.bigint "task_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "day_id", null: false
